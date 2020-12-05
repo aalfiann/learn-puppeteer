1a897d7dae74a3c15c2e5cc2dd519503cde7ffe0
M ABD AZIZ ALFIAN <aalfiann@gmail.com>
2020-12-05 12:11:21 +0700

    - update

---
+++ src/basic.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto('https://example.com')
+  await page.screenshot({ path: 'screenshots/basic.png' })
+  await browser.close()
+})()
